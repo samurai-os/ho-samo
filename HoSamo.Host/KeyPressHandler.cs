35a23ccf2132a1c2f235caf9fcc2ed5a9fd8db73
Paul Buzakov <paulbuzakov@gmail.com>
2025-06-13 19:21:48 +0500

    feat: Added the key press handler to open the Alacritty.
    Signed-off-by: Paul Buzakov <paulbuzakov@gmail.com>

---
+++ HoSamo.Host/KeyPressHandler.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using Serilog;
+using SharpHook;
+using SharpHook.Data;
+
+namespace HoSamo.Host;
+
+public static class KeyPressHandler
+{
+    public static void Handle(HookEventArgs e)
+    {
+        try
+        {
+            var hashCode = e.GetHashCode();
+
+            if (e.RawEvent.Mask is SharpHook.Data.EventMask.LeftMeta or SharpHook.Data.EventMask.RightMeta &&
+                e.RawEvent.Keyboard.KeyCode == KeyCode.VcEnter)
+            {
+                Log.Debug($"[{hashCode}] > Key Pressed: {e.RawEvent}");
+                Log.Information($"[{hashCode}] > Enter key pressed");
+
+                Process.Start("open", "-na /Applications/Alacritty.app/Contents/MacOS/alacritty");
+            }
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "An error occurred while handling key press event");
+        }
+        finally
+        {
+            Log.Debug($"[{e.GetHashCode()}] > Key Pressed: {e.RawEvent}");
+        }
+    }
+}
---
+++ HoSamo.Host/Program.cs
@@ -0,0 +1,27 @@
+﻿using HoSamo.Host;
+using Serilog;
+using SharpHook.Reactive;
+
+try
+{
+    Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Error()
+        .WriteTo.File("logs/ho-samo-.log", rollingInterval: RollingInterval.Day)
+        .CreateLogger();
+
+    Log.Information("HoSamo.Host started");
+
+    var hook = new SimpleReactiveGlobalHook();
+    hook.KeyPressed.Subscribe(KeyPressHandler.Handle);
+
+    hook.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "An unhandled exception occurred during HoSamo.Host execution");
+}
+finally
+{
+    Log.Information("HoSamo.Host stopped");
+    await Log.CloseAndFlushAsync();
+}
